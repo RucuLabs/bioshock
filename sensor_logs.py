9f4e43845599ff71681fc1b2885c823156b43f4f
cristian <cristiancarrion00@gmail.com>
2023-06-12 16:47:23 -0400

    added write to csv file logic

--- sensor_logs.py
+++ sensor_logs.py
@@ -2,15 +2,32 @@
 
 import time
 import Adafruit_DHT
+import csv
+from os.path import exists
 
 #Revisar documentación para ver sensores soportados por adafruit
 sensor = Adafruit_DHT.AM2302
 #pin de data del sensor
 pin = 4
+#intervalo de tiempo entre registro
+interval = 5
+n_regitros = 10
 
-#imprime humedad y temp
-while True:
+
+log_exists = exists('data/sensor_logs.csv')
+if (not log_exists):
+
+    header = ['timestamp', 'temperature', 'humidity']
+    with open('data/sensor_logs.csv', 'w', encoding='UTF-8') as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
+
+for i in n_regitros:
+
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-    print(humidity, temperature)
-    time.sleep(3)
-
+    data = [timestamp, temperature, humidity]
+    with open('data/sensor_logs.csv', 'w', encoding='UTF-8') as f:
+        writer = csv.writer(f)
+        writer.writerow(data)
+    time.sleep(interval)
