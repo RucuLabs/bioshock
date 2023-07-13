1fedf5f64f094c0728e57303ab175dc5c15fec21
cristian <cristiancarrion00@gmail.com>
2023-07-13 14:07:40 -0400

    Ds18b20 test

--- sensor_logs.py
+++ sensor_logs.py
@@ -3,10 +3,12 @@
 import time
 import Adafruit_DHT
 import csv
+from w1thermsensor import W1ThermSensor
 from os.path import exists
 
 #Revisar documentación para ver sensores soportados por adafruit
 sensor = Adafruit_DHT.AM2302
+sensor2 = W1ThermSensor()
 #pin de data del sensor
 pin = 4
 #intervalo de tiempo entre registro
@@ -26,6 +28,8 @@ for i in n_regitros:
 
     timestamp = time.strftime("%Y%m%d-%H%M%S")
     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    temp2 = sensor2.get_temperature()
+    print(temp2)
     data = [timestamp, temperature, humidity]
     with open('data/sensor_logs.csv', 'w', encoding='UTF-8') as f:
         writer = csv.writer(f)
