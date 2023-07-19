611d3d6b4f666adbc136498fc9e5f7229591cc8c
cristian <cristiancarrion00@gmail.com>
2023-07-19 14:54:53 -0400

    onewire test

--- sensor_test.py
+++ sensor_test.py
@@ -1,10 +1,11 @@
 import time
 import board
 import adafruit_dht
+from adafruit_onewire.bus import OneWireBus
 
 # Initial the dht device, with data pin connected to:
 dhtDevice = adafruit_dht.DHT22(board.D18)
-
+ow_bus = OneWireBus(board.D5)
 # you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
 # This may be necessary on a Linux single board computer like the Raspberry Pi,
 # but it will not work in CircuitPython.
@@ -16,6 +17,9 @@ while True:
         temperature_c = dhtDevice.temperature
         temperature_f = temperature_c * (9 / 5) + 32
         humidity = dhtDevice.humidity
+        devices = ow_bus.scan()
+        for device in devices:
+            print("ROM = {} \tFamily = 0x{:02x}".format([hex(i) for i in device.rom], device.family_code))
         print(
             "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
                 temperature_f, temperature_c, humidity
