13fb67e61e77b212c172c6b20dacdb219c55e692
cristian <cristiancarrion00@gmail.com>
2023-07-19 14:27:47 -0400

    blinka test

--- sensor_test.py
+++ sensor_test.py
@@ -1,11 +1,34 @@
-import Adafruit_DHT
-from w1thermsensor import W1ThermSensor
+import time
+import board
+import adafruit_dht
 
-SENSOR_1 = Adafruit_DHT.AM2302
-SENSOR_2 = W1ThermSensor()
-SENSOR_1_PIN = 5
+# Initial the dht device, with data pin connected to:
+dhtDevice = adafruit_dht.DHT22(board.D18)
 
-humidity, temperature = Adafruit_DHT.read_retry(SENSOR_1, SENSOR_1_PIN)
-inner_temperature = SENSOR_2.get_temperature()
+# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
+# This may be necessary on a Linux single board computer like the Raspberry Pi,
+# but it will not work in CircuitPython.
+# dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
 
-print(humidity, temperature, inner_temperature)
+while True:
+    try:
+        # Print the values to the serial port
+        temperature_c = dhtDevice.temperature
+        temperature_f = temperature_c * (9 / 5) + 32
+        humidity = dhtDevice.humidity
+        print(
+            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
+                temperature_f, temperature_c, humidity
+            )
+        )
+
+    except RuntimeError as error:
+        # Errors happen fairly often, DHT's are hard to read, just keep going
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+    except Exception as error:
+        dhtDevice.exit()
+        raise error
+
+    time.sleep(2.0)
