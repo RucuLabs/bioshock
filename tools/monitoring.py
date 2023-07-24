996773d22e457b8e86d2a859bcf7c9b74c6c481b
iPolarisu <elpolarisu@gmail.com>
2023-07-23 23:00:02 -0400

    chore: update cam folder creation

--- tools/monitoring.py
+++ tools/monitoring.py
@@ -8,13 +8,15 @@ DS18X20_id = '28-3c01d607a2d3'
 def start_monitoring(monitoring_name, monitoring_path, cams, resolution, num_photos, interval):
     data_path = f'{monitoring_path}/data.csv'
     pictures_path = f'{monitoring_path}/pictures/'
+    for cam in cams:
+        os.mkdir(f"{monitoring_path}/{cam}/")
     with open(data_path, 'w', newline='') as data_file:
         writer = csv.writer(data_file)
         writer.writerow(HEADERS)
         for i in range(num_photos):
             timestamp = time.strftime("%Y%m%d-%H%M%S")
             for cam in cams:
-                cameras.take_picture(pictures_path, resolution, cam, str(i))
+                cameras.take_picture(f"{monitoring_path}/{cam}/", resolution, cam, str(i))
             humidity, temperature = AM2302.humidity, AM2302.temperature
             inner_temperature = '{:.3f}'.format(ds18x20.gettemp(DS18X20_id)/float(1000))
             row = [i, timestamp, temperature, humidity, inner_temperature, str(i)+'.jpg']
