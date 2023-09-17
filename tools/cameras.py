2a58bd6c7b61b697391e479eada8e992133cf15f
iPolarisu <elpolarisu@gmail.com>
2023-09-17 16:48:20 -0300

    feat: add tested open cv camera fxs

--- tools/cameras.py
+++ tools/cameras.py
@@ -1,4 +1,62 @@
 import os, subprocess
+import cv2
+
+MAX_CAM_PORTS = 10
+
+def list_ports():
+    """
+    Test the ports and returns a tuple with the available ports and the ones that are working.
+    """
+    non_working_ports = []
+    dev_port = 0
+    working_ports = []
+    available_ports = []
+    while len(non_working_ports) < MAX_CAM_PORTS:
+        camera = cv2.VideoCapture(dev_port)
+        if not camera.isOpened():
+            non_working_ports.append(dev_port)
+            print("Port %s is not working." %dev_port)
+        else:
+            is_reading, img = camera.read()
+            w = camera.get(3)
+            h = camera.get(4)
+            if is_reading:
+                print("Port %s is working and reads images (%s x %s)" %(dev_port,h,w))
+                working_ports.append(dev_port)
+            else:
+                print("Port %s for camera ( %s x %s) is present but does not reads." %(dev_port,h,w))
+                available_ports.append(dev_port)
+        dev_port +=1
+    return available_ports,working_ports,non_working_ports
+
+def capture_images(working_ports, pictures_path, picture_name):
+
+    for port in working_ports:
+
+        port_path = f"{pictures_path}/cam{str(port)}"
+        os.makedirs(port_path, exist_ok=True)
+
+        camera = cv2.VideoCapture(port)
+        # camera.set(cv2.CAP_PROP_FRAME_WIDTH, w)
+        # camera.set(cv2.CAP_PROP_FRAME_HEIGHT, h)
+
+        if not camera.isOpened():
+            print(f"Can't open camera at /video{str(port)}")
+            continue
+
+        ret, frame = camera.read()
+        if ret:
+            picture_file_name = f"{picture_name}.jpg"
+            image_path = os.path.join(port_path, picture_file_name)
+            cv2.imwrite(image_path, frame)
+            print(f"Captured image: {image_path}")
+        else:
+            print(f"Can't take picture at /video{str(port)}")
+
+        camera.release()
+
+
+### DEPRECATED
 
 # detect: void -> list / false
 # indicates if videocams are connected to the rpi
