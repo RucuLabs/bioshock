ba00c289a43caa8a7e8f7579126c57493b4c6b73
ccarrions <cristiancarrion00@gmail.com>
2023-09-22 11:37:38 -0300

    feat: ds18b sensor test fix 2

--- tools/ds18x20.py
+++ tools/ds18x20.py
@@ -5,7 +5,7 @@ def gettemp():
   mytemp = ''
   base_dir = '/sys/bus/w1/devices/'
   device_folder = glob.glob(base_dir + '28*')[0]
-  device_file = device_folder + 'w1_slave'
+  device_file = device_folder + '/w1_slave'
   
   f = open(device_file, 'r')
   line = f.readline() # read 1st line
