5f16c8353f9889ce75f4fe11caeab02ce3b79b96
iPolarisu <elpolarisu@gmail.com>
2023-09-22 13:27:26 -0300

    feat: add monitoring cycle fx

--- tools/monitoring.py
+++ tools/monitoring.py
@@ -34,11 +34,11 @@ def start_monitoring(monitoring_path, working_ports, interval):
             
             # ADD SENSORS
 
-            humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
-            inner_temperature = '{:.3f}'.format(ds18x20.gettemp()/float(1000))
-            # humidity = 'humidity'
-            # temperature = 'temperature'
-            # inner_temperature = 'inner_temperature'
+            #humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+            #inner_temperature = '{:.3f}'.format(ds18x20.gettemp()/float(1000))
+            humidity = 'humidity'
+            temperature = 'temperature'
+            inner_temperature = 'inner_temperature'
 
             # write the registry
             row = [i, timestamp, temperature, humidity, inner_temperature]
@@ -48,3 +48,29 @@ def start_monitoring(monitoring_path, working_ports, interval):
 
             # wait for next iteration
             time.sleep(interval)
+
+def monitoring_cycle(monitoring_path, working_ports, interval):
+    data_path = f'{monitoring_path}/data.csv'
+    pictures_path = f'{monitoring_path}/pictures'
+    
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
+            
+    cameras.capture_images(working_ports=working_ports, pictures_path=pictures_path, picture_name=str(i))
+            
+    # ADD SENSORS
+
+    # humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+    # inner_temperature = '{:.3f}'.format(ds18x20.gettemp()/float(1000))
+    
+    humidity = 'humidity'
+    temperature = 'temperature'
+    inner_temperature = 'inner_temperature'
+
+    # write the registry
+    row = [i, timestamp, temperature, humidity, inner_temperature]
+    writer.writerow(row)
+
+    i += 1
+
+    # wait for next iteration
+    time.sleep(interval)
