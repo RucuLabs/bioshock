becffce36cac9fd6950dc86ea71b652ee26ceaf3
sebasaav <elpolarisu@gmail.com>
2023-09-22 22:58:30 -0300

    feat: handle errors app pyqt

---
+++ tools/errors.py
@@ -0,0 +1,3 @@
+NOT_VALID_PROJECT_NAME = "Not valid project name, avoid using spaces or weird characters."
+NOT_VALID_INTERVAL = "Not valid interval value, use whole numbers greater than 0."
+PROJECT_ALREADY_EXISTS = "Project already exists, use another name."
