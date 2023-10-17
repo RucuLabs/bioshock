8ae7bb3a705fd40082a7ce2d1ef4f74cd9fe319d
iPolarisu <elpolarisu@gmail.com>
2023-10-17 17:36:14 -0300

    feat: add two ds18x20 sensors at the same time

--- tools/ds18x20.py
+++ tools/ds18x20.py
@@ -1,21 +1,33 @@
 #!/usr/bin/python
 import glob
-
+import os
 def gettemp():
   mytemp = ''
   base_dir = '/sys/bus/w1/devices/'
-  device_folder = glob.glob(base_dir + '28*')[0]
-  device_file = device_folder + '/w1_slave'
-  
-  f = open(device_file, 'r')
-  line = f.readline() # read 1st line
-  crc = line.rsplit(' ',1)
-  crc = crc[1].replace('\n', '')
-  if crc=='YES':
-    line = f.readline() # read 2nd line
-    mytemp = line.rsplit('t=',1)
-  else:
-    mytemp = 123456789
-  f.close()
+  # device_folder = glob.glob(base_dir + '28*')[0]
+  temperatures = []
+  for device_folder in glob.glob(base_dir + '28*'):
+    device_file = device_folder + '/w1_slave'
+    
+    f = open(device_file, 'r')
+    line = f.readline() # read 1st line
+    crc = line.rsplit(' ',1)
+    crc = crc[1].replace('\n', '')
+    if crc=='YES':
+      line = f.readline() # read 2nd line
+      mytemp = line.rsplit('t=',1)
+    else:
+      mytemp = 123456789
+    f.close()
+    temperatures.append(int(mytemp[1]))
 
-  return int(mytemp[1])
+  return temperatures
+
+def getTempSensors():
+  sensors = []
+  base_dir = '/sys/bus/w1/devices/'
+  for filename in os.listdir(base_dir):
+    if filename == 'w1_bus_master1':
+      continue
+    sensors.append(filename)
+    return sensors
--- tools/monitoring.py
+++ tools/monitoring.py
@@ -1,17 +1,19 @@
-# import tools.ds18x20 as ds18x20
+import tools.ds18x20 as ds18x20
 import tools.cameras as cameras
 import csv, time
-# import Adafruit_DHT
+import Adafruit_DHT
 
 HEADERS = ['iteration', 
             'time', 
             'temperature', 
             'humidity', 
-            'inner_temperature']
+            'inner_temperature',
+            'inner_temperature2']
 
 # DHT_PIN = 18
 # DHT_SENSOR = Adafruit_DHT.DHT22
 # DS18X20_id = '28-3c01d607a2d3'
+DS18X20_ids = ds18x20.getTempSensors()
 
 def monitoring_cycle(monitoring_path, working_ports, iteration):
 
@@ -24,11 +26,12 @@ def monitoring_cycle(monitoring_path, working_ports, iteration):
             
     # ADD SENSORS
     # humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
-    # inner_temperature = '{:.3f}'.format(ds18x20.gettemp()/float(1000))
+    inner_temperature = '{:.3f}'.format(ds18x20.gettemp()[0]/float(1000))
+    inner_temperature2 = '{:.3f}'.format(ds18x20.gettemp()[1]/float(1000))
     
     humidity = '0'
     temperature = '0'
-    inner_temperature = '0'
+    # inner_temperature = '0'
     timestamp = time.strftime("%d%m%Y-%H%M%S")
 
     with open(data_path, mode='a', newline='', encoding='utf-8') as data_csv:
@@ -43,6 +46,7 @@ def monitoring_cycle(monitoring_path, working_ports, iteration):
                     'temperature' : temperature, 
                     'humidity' : humidity, 
                     'inner_temperature' : inner_temperature,
+                    'inner_temperature2' : inner_temperature2
         } 
         
         writer_csv.writerow(row)
