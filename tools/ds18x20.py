042ef798a8ecd7b8387a4bcc50e9280b31a049d5
ccarrions <cristiancarrion00@gmail.com>
2023-11-23 11:45:56 -0300

    feat: add try except to d18b module

--- tools/ds18x20.py
+++ tools/ds18x20.py
@@ -7,19 +7,22 @@ def gettemp():
   # device_folder = glob.glob(base_dir + '28*')[0]
   temperatures = []
   for device_folder in glob.glob(base_dir + '28*'):
-    device_file = device_folder + '/w1_slave'
-    
-    f = open(device_file, 'r')
-    line = f.readline() # read 1st line
-    crc = line.rsplit(' ',1)
-    crc = crc[1].replace('\n', '')
-    if crc=='YES':
-      line = f.readline() # read 2nd line
-      mytemp = line.rsplit('t=',1)
-    else:
-      mytemp = -1
-    f.close()
-    temperatures.append(int(mytemp[1]))
+    try: 
+      device_file = device_folder + '/w1_slave'
+      
+      f = open(device_file, 'r')
+      line = f.readline() # read 1st line
+      crc = line.rsplit(' ',1)
+      crc = crc[1].replace('\n', '')
+      if crc=='YES':
+        line = f.readline() # read 2nd line
+        mytemp = line.rsplit('t=',1)
+      else:
+        mytemp = -1
+      f.close()
+      temperatures.append(int(mytemp[1]))
+    except:
+      temperatures.append(-1)
 
   return temperatures
 
